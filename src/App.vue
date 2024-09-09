2ac230797a71f9c6e4413f79d10f66575caf1410
Ezerike Caleb Chikere <cezerike@gmail.com>
2024-09-09 17:39:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data () {
+    return {
+      name: 'John Doe',
+      status: false,
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">user is active</p>
+  <p v-if="status">user is active</p>
+</template>
+
