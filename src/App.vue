ffa5003e3d7497732eeb71da7975085c542702b3
Ezerike Caleb Chikere <cezerike@gmail.com>
2024-09-11 19:51:07 +0100

    'if-else & list items'

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@ export default {
     return {
       name: 'John Doe',
       status: 'active',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://google.com'
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
     }
   }
 }
@@ -14,5 +27,14 @@ export default {
   <p v-if="status === 'active'">user is active</p>
   <p v-else-if="status === 'pending'">user is pending</p>
   <p v-else>user is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a v-bind:href="link">Click for Google</a>
+<br />
+  <button v-on:click="toggleStatus">Change Status</button>
 </template>
 
